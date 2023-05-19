6b082d389c15ed1f75358f5b4002c5ea6ab18a14
anubhavmishra800 <127845673+anubhavmishra800@users.noreply.github.com>
2023-05-19 11:58:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
-function threeSum(arr, target) {
-// write your code here
+function decimalToBinary(decimalNumber) {
   
-}
+  let binaryNumber = "";
+  let quotient = decimalNumber;
 
-module.exports = threeSum;
+  // perform division until quotient is zero
+  while (quotient !== 0) {
+    // calculate remainder
+    let remainder = quotient % 2;
+
+    // add remainder to binary number
+    binaryNumber = remainder.toString() + binaryNumber;
+
+    // divide quotient by 2
+    quotient = Math.floor(quotient / 2);
+  }
+
+  // return binary number as a string
+  return binaryNumber;
+}
